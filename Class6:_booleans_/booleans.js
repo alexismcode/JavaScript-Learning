ea4ce67745b8ca0d2307d82860438eba1d8b45d9
alexismcode <alexismcode@gmail.com>
2022-05-31 23:20:28 -0500

    Add sixth class

---
+++ Class6:_booleans_/booleans.js
@@ -0,0 +1,12 @@
+let V = true;
+let F = false;
+let verdadero = Boolean(true); 
+
+console.log(V, F);
+console.log(typeof V, typeof F);
+console.log(verdadero);
+
+console.log(Boolean(0));
+console.log(Boolean(-7));
+console.log(Boolean(""));
+console.log(Boolean(" "));
