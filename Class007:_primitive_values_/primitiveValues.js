2c5451ab54e036b9db119f77b80565b48cd94fb9
alexismcode <alexismcode@gmail.com>
2022-06-16 22:14:36 -0500

    Add seventh class

---
+++ Class007:_primitive_values_/primitiveValues.js
@@ -0,0 +1,11 @@
+//missing value without defining it
+let indef;
+console.log(indef);
+
+//missing value defined
+let nulo = null;
+console.log(nulo);
+
+// Not a Number
+let notANumber = "hello" * 3.7;
+console.log(notANumber)
