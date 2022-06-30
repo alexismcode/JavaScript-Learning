e3accb0c8df425a2bea49beb8c2009b57c2551bf
alexismcode <alexismcode@gmail.com>
2022-06-30 18:18:05 -0500

    Add twenty sixth class

---
+++ Class026:_math_object_/mathObject.js
@@ -0,0 +1,17 @@
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.abs(-7.8));
+console.log(Math.ceil(7.8));
+console.log(Math.floor(7.8));
+
+console.log(Math.round(7.49));
+console.log(Math.round(7.8));
+//Square root
+console.log(Math.sqrt(81));
+//Empowerment
+console.log(Math.pow(2,5))
+//-1(negative) 0 1(positive)
+console.log(Math.sign(-0.4));
+//Random numbers
+console.log(Math.random());
+console.log(Math.round(Math.random()*50));
