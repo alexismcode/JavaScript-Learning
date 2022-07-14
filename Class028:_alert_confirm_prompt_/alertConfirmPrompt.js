79adf1d8d4e54ad709e0d9c8dda5dbfa882c8c86
alexismcode <alexismcode@gmail.com>
2022-07-13 19:22:29 -0500

    Add twenty eighth class

---
+++ Class028:_alert_confirm_prompt_/alertConfirmPrompt.js
@@ -0,0 +1,12 @@
+console.log(window);
+//alert("Hello this is an alert");
+//confirm("Hello this is a confirmation");
+//prompt("Hello this is prompt and allows the user to enter a value");
+
+let notification = alert("Hello this is an alert");
+let confirmation = confirm("Hello this is a confirmation");
+let message = prompt("Hello this is prompt and allows the user to enter a value");
+
+console.log(notification);
+console.log(confirmation);
+console.log(message);
